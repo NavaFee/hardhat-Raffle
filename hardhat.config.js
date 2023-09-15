08f3de0e717a062e426706ce964d63a2ed11baf0
NavaFee <1457660144@qq.com>
2023-09-15 10:16:45 +0800

    contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,5 +8,40 @@ require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [SEPOLIA_PRIVATE_KEY],
+            chainId: 11155111,
+            blockConfirmations: 6,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            // hardhat provide accounts
+            chainId: 31337,
+        },
+    },
     solidity: "0.8.19",
+    nameAccounts: {
+        deployer: {
+            default: 0,
+        },
+        palyer: {
+            default: 1,
+        },
+    },
+    gasReporter: {
+        enabled: false,
+        //outputFile: "gas-report.txt",
+        //gasPrice: 21,
+        noColors: true,
+        currency: "CNY",
+        //   coinmarketcap: COINMARKETCAP_API_KEY,
+        //token: "ETH",
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+        timeout: 600000,
+    },
 }
