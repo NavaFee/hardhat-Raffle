57c7d9f755d57ecb2a01fab38f97281fe074047c
NavaFee <1457660144@qq.com>
2023-09-17 12:24:50 +0800

    unit-test

--- test/unit/Raffle.test.js
+++ test/unit/Raffle.test.js
@@ -90,9 +90,78 @@ const { getBytes, isBytesLike } = require("ethers")
                   await network.provider.send("evm_mine", [])
                   await raffle.performUpkeep("0x")
                   const raffleState = await raffle.getRaffleState()
-                  const { upkeepNeeded } = await raffle.checkUpkeep("0x")
-                  assert(!upkeepNeeded)
+                  const { upkeepNeeded } = await raffle.checkUpkeep.staticCall(
+                      "0x"
+                  )
+                  assert.equal(upkeepNeeded, false)
                   assert.equal(raffleState.toString(), "1")
+              })
+              it("returns false if enough time has't passed", async () => {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  await network.provider.send("evm_increaseTime", [
+                      Number(interval) - 5,
+                  ]) // use a higher number here if this test fails
+                  await network.provider.request({
+                      method: "evm_mine",
+                      params: [],
+                  })
+                  const { upkeepNeeded } = await raffle.checkUpkeep.staticCall(
+                      "0x"
+                  ) // upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers)
+                  assert(!upkeepNeeded)
+              })
+              it("returns true if enough time has passed, has players, eth, and is open", async () => {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  await network.provider.send("evm_increaseTime", [
+                      Number(interval) + 1,
+                  ])
+                  await network.provider.request({
+                      method: "evm_mine",
+                      params: [],
+                  })
+                  const { upkeepNeeded } = await raffle.checkUpkeep.staticCall(
+                      "0x"
+                  ) // upkeepNeeded = (timePassed && isOpen && hasBalance && hasPlayers)
+                  assert(upkeepNeeded)
+              })
+          })
+          describe("performUpkeep", function () {
+              it("it can only run if checkupkeep is true", async function () {
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  await network.provider.send("evm_increaseTime", [
+                      Number(interval) + 1,
+                  ])
+                  await network.provider.request({
+                      method: "evm_mine",
+                      params: [],
+                  })
+                  const tx = await raffle.performUpkeep("0x")
+                  assert(tx)
+              })
+              it("reverts when checkupkeep is false", async () => {
+                  await expect(
+                      raffle.performUpkeep("0x")
+                  ).to.be.revertedWithCustomError(
+                      raffle,
+                      "Raffle__UPKeepNotNeeded"
+                  )
+              })
+              it("updates the raffle state and emits a event, and calls the vrf coordinator", async () => {
+                  // Too many asserts in this test!
+                  await raffle.enterRaffle({ value: raffleEntranceFee })
+                  await network.provider.send("evm_increaseTime", [
+                      Number(interval) + 1,
+                  ])
+                  await network.provider.request({
+                      method: "evm_mine",
+                      params: [],
+                  })
+                  const txResponse = await raffle.performUpkeep("0x") // emits requestId
+                  const txReceipt = await txResponse.wait(1) // waits 1 block
+                  const raffleState = await raffle.getRaffleState() // updates state
+                  const requestId = txReceipt.logs[1].args[0]
+                  assert(Number(requestId) > 0)
+                  assert(raffleState == 1) // 0 = open, 1 = calculating
               })
           })
       })
